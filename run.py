7f51c3a6a4997590e75d67a260a49435659eaeb1
jamie120 <jamie120@hotmail.com>
2021-01-05 09:28:36 +0000

    deployment to heroku

---
+++ run.py
@@ -0,0 +1,34 @@
+import os
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have recieved your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
